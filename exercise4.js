cbf9f9d059887dbb6594e0fd7a7183e667cbdc29
starkie001 <starkwanos@hotmail.com>
2025-05-10 18:25:57 +1200

    updates for greeting exercise4

--- exercise4.js
+++ exercise4.js
@@ -1,20 +1,25 @@
 function sum(a,b) {
-    console.log(a+b)
+    return a+b
 }
 
 function subtract(a,b) {
-    console.log(a-b)
+    return a-b
 }
 
 function multiply(a,b) {
-    console.log(a*b)
+    return a*b
 }
 
 function divide(a,b) {
-    console.log(a/b)
+    return a/b
+}
+
+function greet(name) {
+    return "Hello " + name
 }
 
 console.log(sum(9,3))
 console.log(subtract(9,3))
 console.log(multiply(9,3))
 console.log(divide(9,3))
+console.log(greet("Davinder"))
