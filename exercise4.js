0e5b23696793497b4e8b122ec55ce45fa1654842
starkie001 <starkwanos@hotmail.com>
2025-05-10 16:40:20 +1200

    updated exercise 4

--- exercise4.js
+++ exercise4.js
@@ -1,4 +1,20 @@
 function sum(a,b) {
     console.log(a+b)
 }
-console.log(sum(5,1))
+
+function subtract(a,b) {
+    console.log(a-b)
+}
+
+function multiply(a,b) {
+    console.log(a*b)
+}
+
+function divide(a,b) {
+    console.log(a/b)
+}
+
+console.log(sum(9,3))
+console.log(subtract(9,3))
+console.log(multiply(9,3))
+console.log(divide(9,3))
