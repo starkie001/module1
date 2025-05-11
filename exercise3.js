b6c969570eaaf0581c01c151973a6662bd77776f
starkie001 <starkwanos@hotmail.com>
2025-05-11 21:48:24 +1200

    update exercise3 external script

---
+++ exercise3.js
@@ -0,0 +1,11 @@
+
+function changeMe1(){
+    document.getElementById("column1").style.background = "green"
+    document.getElementById("heading1").style.color = "red"
+    document.getElementById("heading1").textContent = document.getElementById("headswap1").value
+}
+function changeMe2(){
+    document.getElementById("column2").style.background = "green"
+    document.getElementById("heading2").style.color = "red"
+    document.getElementById("heading2").textContent = document.getElementById("headswap2").value
+}
